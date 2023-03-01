54556335272268746f97f0e4648cdbf5c5a002fd
bernathmark <nitro_5_bernath_mark@outlook.hu>
2023-03-01 16:08:44 +0100

    todo app logic

---
+++ lotto_app/app.js
@@ -0,0 +1,8 @@
+const addBtn = document.getElementById("add-btn");
+const playBtn = document.getElementById("play-btn");
+const userNumberList = document.getElementById("user-numbers");
+const machineNumbersList = document.getElementById("machine-numbers");
+let userNumbers = [];
+let machineNumbers = [];
+let isEnough = false;
+
