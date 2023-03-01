a26385e9bf373e072bf6eab98832f8a9ecaecddd
bernathmark <nitro_5_bernath_mark@outlook.hu>
2023-03-01 10:45:25 +0100

    dom basics

---
+++ DOM_basics/index.js
@@ -0,0 +1,14 @@
+//The HTML DOM is a standard for how to get, change, add, or delete HTML elements.
+// HTML DOM (Document Object Model)
+// -The HTML elements as objects
+// -The properties of all HTML elements
+// -The methods to access all HTML elements
+// -The events for all HTML elements
+
+//1.getElementById Method and innerHTML Property
+document.getElementById('demo-one').innerHTML = 'Hello World!'
+
+//Finding HTML elements
+//getElementById()
+//getElementsByTagName()
+//getElementsByClassName()
