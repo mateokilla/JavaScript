95e1773a01d91f611bb33c60154a10a3a39442b2
bernathmark <113900292+mateokilla@users.noreply.github.com>
2023-03-07 16:49:16 +0100

    programozási tételek gyakorlása

--- JS_Basics/index.js
+++ JS_Basics/index.js
@@ -46,7 +46,7 @@ document.write("</table>");
 document.write("<hr>");
 
 //digiatlis ora
-const dora = document.getElementById('dora');
+const dora = document.getElementById("dora");
 
 function oraKiir() {
   datum = new Date();
@@ -55,7 +55,71 @@ function oraKiir() {
   perc = datum.getMinutes();
   mp = datum.getSeconds();
 
-  dora.innerHTML = ora + ':' + perc + ':' + mp
+  dora.innerHTML = ora + ":" + perc + ":" + mp;
 }
-setInterval('oraKiir()', 1000)
-oraKiir()
+setInterval("oraKiir()", 1000);
+oraKiir();
+
+//egy 10 elemű random tömb generálás
+
+function randomTombGenerator(felsoHatar, alsoHatar, tombElemSzam) {
+  let randomSzamokHalmaza = [];
+
+  for (let i = 0; i < tombElemSzam; i++) {
+    randomSzamokHalmaza[i] =
+      Math.floor(Math.random() * (felsoHatar - alsoHatar)) + alsoHatar;
+  }
+  return randomSzamokHalmaza;
+}
+
+let mintaTomb = randomTombGenerator(10, 1, 5);
+
+//1.Mennyi a mintaTomb összege és átlaga?
+function atlagSzamito(tomb) {
+  let osszeg = 0;
+  let atlag = 0;
+
+  for (let i = 0; i < tomb.length; i++) {
+    osszeg += tomb[i];
+  }
+  atlag = parseInt(osszeg / tomb.length);
+
+  return atlag;
+}
+
+document.write(atlagSzamito(mintaTomb));
+
+document.write("<hr>");
+
+//2.Kiválogatás: Hány darab páros szám van a tömbben?
+function parosSzamlalo(tomb) {
+  let parosSzamokSzama = 0;
+
+  for (let i = 0; i < tomb.length; i++) {
+    if (tomb[i] % 2 == 0) {
+      parosSzamokSzama++;
+    }
+  }
+  return parosSzamokSzama;
+}
+document.write("A mintaTomb elemei a következőek: " + mintaTomb);
+document.write("<br>");
+document.write(
+  "A páros számok száma a mintaTomb-ben: " + parosSzamlalo(mintaTomb)
+);
+
+//3.Melyik a legnagyobb elem a tömbben?
+
+function maximumErtek(tomb) {
+  let maxIndex = 0;
+
+  for (let i = 0; i < tomb.length; i++) {
+    if (tomb[i] > maxIndex) {
+      maxIndex = tomb[i];
+    }
+  }
+
+  return maxIndex;
+}
+document.write("<br>");
+document.write("A mintaTomb legynagyobb eleme: " + maximumErtek(mintaTomb));
